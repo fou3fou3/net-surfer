0afbad3e9bc471fd389a1eeaa00d21921602787d
fou3fou3 <foxedex29@gmail.com>
2024-05-25 09:11:09 +0100

    json file && db initialization

---
+++ init.py
@@ -0,0 +1,13 @@
+import json
+
+def init_crawled_list(file_name: str = 'src/json_data/crawled_links_list.json') -> None:
+    with open(file_name, 'w') as json_file:
+        json.dump({'crawled_links': []}, json_file, indent=4)
+
+def init_seed_list(file_name: str = 'src/json_data/seed_list.json') -> None:
+    with open(file_name, 'w') as json_file:
+        json.dump({'seed_list': []}, json_file, indent=4)
+
+
+init_seed_list()
+init_crawled_list()
