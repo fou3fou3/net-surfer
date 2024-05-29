ac4b4630a6be0da4948fe7090c81f0fa5be3f20e
fou3fou3 <foxedex29@gmail.com>
2024-05-29 13:37:00 +0100

    db init to the init file so we avoid running the create table query each time

--- init.py
+++ init.py
@@ -1,4 +1,11 @@
-import json
+import json, sqlite3
+from sqlite3 import Connection
+
+connection = sqlite3.connect('src/database/net_surfer.db')
+
+
+# Be sure to run this file before starting the crawler or making any database operations as it cleans the json files
+# and initializes database tables
 
 def init_crawled_list(file_name: str = 'src/json_data/crawled_links.json') -> None:
     with open(file_name, 'w') as json_file:
@@ -9,5 +16,61 @@ def init_seed_list(file_name: str = 'src/json_data/seed_list.json') -> None:
         json.dump({'seed_list': []}, json_file, indent=4)
 
 
-init_seed_list()
-init_crawled_list()
+def init_crawled_links_table(conn: Connection):
+	try:
+		cursor = conn.cursor()
+
+		cursor.execute('''CREATE TABLE IF NOT EXISTS crawled_pages (
+                            page_url TEXT PRIMARY KEY,
+                            page_content TEXT NOT NULL,
+                            page_title TEXT NOT NULL,
+                            parent_link TEXT,
+                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                            FOREIGN KEY (parent_link) REFERENCES crawled_pages(page_url)
+                            );
+                      ''')
+
+		conn.commit()
+
+	except Exception as e:
+		print(f'Error initializing the crawled_pages table: {e}')
+
+
+def init_words_table(conn: Connection):
+	try:
+		cursor = conn.cursor()
+
+		cursor.execute('''CREATE TABLE IF NOT EXISTS word_frequencies (
+                            page_url TEXT,
+                            word TEXT NOT NULL,
+                            frequency INTEGER NOT NULL,
+                            PRIMARY KEY (page_url, word),
+                            FOREIGN KEY (page_url) REFERENCES crawled_pages(page_url)
+                        );
+                      ''')
+
+		conn.commit()
+
+	except Exception as e:
+		print(f'Error initializing the words_frequencies table: {e}')
+
+
+def init_robots_table(conn: Connection):
+	try:
+		cursor = conn.cursor()
+
+		cursor.execute('''CREATE TABLE IF NOT EXISTS robots_txt (
+                            base_url TEXT PRIMARY KEY,
+                            robots_text TEXT NOT NULL
+                            );
+                      ''')
+
+		conn.commit()
+
+	except Exception as e:
+		print(f'Error initializing the robots_txt table: {e}')
+
+
+init_json_functions = [init_crawled_list(), init_seed_list()]
+init_tables_functions = [init_crawled_links_table(conn=connection), init_words_table(connection),
+                         init_words_table(connection)]
--- src/database/db.py
+++ src/database/db.py
@@ -9,16 +9,6 @@ async def add_page_to_db(conn: Connection, page_url: str, page_content: str, pag
         if not page_title:
             page_title = 'NULL'
 
-        cursor.execute('''CREATE TABLE IF NOT EXISTS crawled_pages (
-                            page_url TEXT PRIMARY KEY,
-                            page_content TEXT NOT NULL,
-                            page_title TEXT NOT NULL,
-                            parent_link TEXT,
-                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                            FOREIGN KEY (parent_link) REFERENCES crawled_pages(page_url)
-                            );
-                      ''')
-
         cursor.execute(
             ''' INSERT INTO crawled_pages (page_url, page_content, page_title, parent_link) VALUES (?, ?, ?, ?) ''',
             (page_url, page_content, page_title, parent_link))
@@ -34,15 +24,6 @@ async def add_page_to_db(conn: Connection, page_url: str, page_content: str, pag
 async def add_words_to_db(conn: Connection, page_url: str, words: list[tuple[str, int]]):
     try:
         cursor = conn.cursor()
-
-        cursor.execute('''CREATE TABLE IF NOT EXISTS word_frequencies (
-                            page_url TEXT,
-                            word TEXT NOT NULL,
-                            frequency INTEGER NOT NULL,
-                            PRIMARY KEY (page_url, word),
-                            FOREIGN KEY (page_url) REFERENCES crawled_pages(page_url)
-                        );
-                      ''')
 
         cursor.executemany(
             ''' INSERT INTO word_frequencies (page_url, word, frequency) VALUES (?, ?, ?) ''',
@@ -75,12 +56,6 @@ async def fetch_robots_txt(conn: Connection, base_url: str) -> str | None:
 async def add_robots_txt(conn: Connection, base_url: str, robots_txt: str):
     try:
         cursor = conn.cursor()
-
-        cursor.execute('''CREATE TABLE IF NOT EXISTS robots_txt (
-                            base_url TEXT PRIMARY KEY,
-                            robots_text TEXT NOT NULL
-                            );
-                      ''')
 
         cursor.execute(''' INSERT INTO robots_txt (base_url, robots_text) VALUES (?, ?) ''',
                        (base_url, robots_txt))
