0d23485806020ad3122fcef2642c89bc91328e14
fou3fou3 <foxedex29@gmail.com>
2024-05-29 14:05:52 +0100

    index words done!

--- init.py
+++ init.py
@@ -8,12 +8,12 @@ connection = sqlite3.connect('src/database/net_surfer.db')
 # and initializes database tables
 
 def init_crawled_list(file_name: str = 'src/json_data/crawled_links.json') -> None:
-    with open(file_name, 'w') as json_file:
-        json.dump({'crawled_links': []}, json_file, indent=4)
+	with open(file_name, 'w') as json_file:
+		json.dump({'crawled_links': []}, json_file, indent=4)
 
 def init_seed_list(file_name: str = 'src/json_data/seed_list.json') -> None:
-    with open(file_name, 'w') as json_file:
-        json.dump({'seed_list': []}, json_file, indent=4)
+	with open(file_name, 'w') as json_file:
+		json.dump({'seed_list': []}, json_file, indent=4)
 
 
 def init_crawled_links_table(conn: Connection):
@@ -21,14 +21,14 @@ def init_crawled_links_table(conn: Connection):
 		cursor = conn.cursor()
 
 		cursor.execute('''CREATE TABLE IF NOT EXISTS crawled_pages (
-                            page_url TEXT PRIMARY KEY,
-                            page_content TEXT NOT NULL,
-                            page_title TEXT NOT NULL,
-                            parent_link TEXT,
-                            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                            FOREIGN KEY (parent_link) REFERENCES crawled_pages(page_url)
-                            );
-                      ''')
+							page_url TEXT PRIMARY KEY,
+							page_content TEXT NOT NULL,
+							page_title TEXT NOT NULL,
+							parent_link TEXT,
+							created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+							FOREIGN KEY (parent_link) REFERENCES crawled_pages(page_url)
+							);
+					  ''')
 
 		conn.commit()
 
@@ -41,13 +41,15 @@ def init_words_table(conn: Connection):
 		cursor = conn.cursor()
 
 		cursor.execute('''CREATE TABLE IF NOT EXISTS word_frequencies (
-                            page_url TEXT,
-                            word TEXT NOT NULL,
-                            frequency INTEGER NOT NULL,
-                            PRIMARY KEY (page_url, word),
-                            FOREIGN KEY (page_url) REFERENCES crawled_pages(page_url)
-                        );
-                      ''')
+							page_url TEXT,
+							word TEXT NOT NULL,
+							frequency INTEGER NOT NULL,
+							PRIMARY KEY (page_url, word),
+							FOREIGN KEY (page_url) REFERENCES crawled_pages(page_url)
+						);
+					  ''')
+
+		cursor.execute('''CREATE INDEX IF NOT EXISTS idx_word ON word_frequencies (word);''')
 
 		conn.commit()
 
@@ -60,10 +62,10 @@ def init_robots_table(conn: Connection):
 		cursor = conn.cursor()
 
 		cursor.execute('''CREATE TABLE IF NOT EXISTS robots_txt (
-                            base_url TEXT PRIMARY KEY,
-                            robots_text TEXT NOT NULL
-                            );
-                      ''')
+							base_url TEXT PRIMARY KEY,
+							robots_text TEXT NOT NULL
+							);
+					  ''')
 
 		conn.commit()
 
