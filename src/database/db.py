fa77b1e1e026334f90f2496b84712207cb825c65
fou3fou3 <foxedex29@gmail.com>
2024-05-31 20:35:21 +0100

    tabs-style

--- src/database/db.py
+++ src/database/db.py
@@ -2,42 +2,43 @@ import asyncpg
 from asyncpg import Connection
 
 async def add_page_to_db(conn: Connection, page_url: str, page_content: str, page_title: str, parent_link: str = None):
-    try:
-        if parent_link:
-            await conn.execute(
-                '''INSERT INTO crawled_pages (page_url, page_content, page_title, parent_link) VALUES ($1, $2, $3, $4)''',
-                page_url, page_content, page_title, parent_link)
-        else:
-            await conn.execute(
-                '''INSERT INTO crawled_pages (page_url, page_content, page_title, NULL) VALUES ($1, $2, $3, $4)''',
-                page_url, page_content, page_title)
-        
-    except Exception as e:
-        print(f"\t|- Error adding page {page_url} to the database: {e}")
-        
+	try:
+		if parent_link:
+			await conn.execute(
+				'''INSERT INTO crawled_pages (page_url, page_content, page_title, parent_link) VALUES ($1, $2, $3, $4)''',
+				page_url, page_content, page_title, parent_link)
+		else:
+			await conn.execute(
+				'''INSERT INTO crawled_pages (page_url, page_content, page_title, NULL) VALUES ($1, $2, $3, $4)''',
+				page_url, page_content, page_title)
+		
+	except Exception as e:
+		print(f"\t|- Error adding page {page_url} to the database: {e}")
+		
+		
 async def add_words_to_db(conn: asyncpg.Connection, page_url: str, words: list[tuple[str, int]]):
-    try:
-        async with conn.transaction():
-            for word, freq in words:
-                try:
-                    # Try to update the frequency if the record exists
-                    update_query = '''
-                        UPDATE word_frequencies
-                        SET frequency = word_frequencies.frequency + $1
-                        WHERE page_url = $2 AND word = $3
-                    '''
-                    res = await conn.execute(update_query, freq, page_url, word)
-                    
-                    if res == 'UPDATE 0':
-                        # If no rows were updated, insert the new word with its frequency
-                        insert_query = '''
-                            INSERT INTO word_frequencies (page_url, word, frequency)
-                            VALUES ($1, $2, $3)
-                        '''
-                        await conn.execute(insert_query, page_url, word, freq)
-                
-                except Exception as e:
-                    print(f'|- Error updating word frequency: {e}')
-            
-    except Exception as e:
-        print(f'\t|- Error adding word to the database: {e}')
+	try:
+		async with conn.transaction():
+			for word, freq in words:
+				try:
+					# Try to update the frequency if the record exists
+					update_query = '''
+						UPDATE word_frequencies
+						SET frequency = word_frequencies.frequency + $1
+						WHERE page_url = $2 AND word = $3
+					'''
+					res = await conn.execute(update_query, freq, page_url, word)
+					
+					if res == 'UPDATE 0':
+						# If no rows were updated, insert the new word with its frequency
+						insert_query = '''
+							INSERT INTO word_frequencies (page_url, word, frequency)
+							VALUES ($1, $2, $3)
+						'''
+						await conn.execute(insert_query, page_url, word, freq)
+				
+				except Exception as e:
+					print(f'|- Error updating word frequency: {e}')
+			
+	except Exception as e:
+		print(f'\t|- Error adding word to the database: {e}')
--- src/json_data/json_io.py
+++ src/json_data/json_io.py
@@ -1,5 +1,5 @@
 import json
 
 def load_seed_list(file_name: str = 'json_data/seed_list.json') -> list[str]:
-    with open(file_name, 'r') as json_file:
-        return json.load(json_file)['seed_list']
+	with open(file_name, 'r') as json_file:
+		return json.load(json_file)['seed_list']
